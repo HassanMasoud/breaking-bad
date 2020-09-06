930c16728042a96043c0bdb3d5c042ad100a81f7
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-06 15:08:30 +0300

    Create Search component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/Header";
+import Search from "./components/ui/Search";
 import CharacterGrid from "./components/characters/CharacterGrid";
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <Search />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
